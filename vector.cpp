b37e45a2cc37936117fc732344135e7e9c127843
stmbsnaasmf <43440784+stmbsnaasmf@users.noreply.github.com>
2025-10-29 11:57:30 +0500

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -5,7 +5,7 @@ Vector::Vector(const double x, const double y, const double z) {}
 Vector::Vector(const Vector& other) {}
 Vector& Vector::operator = (const Vector& other) {}
 Vector Vector::operator * (const double scalar) const {}
-double Vector::getDotProduct(const Vector& other, const double theta) const {}
+double Vector::getDotProduct(const Vector& other, const double theta_radians) const {}
 Vector Vector::operator * (const Vector other) const {}	//Cross product
 
 double Vector::getX() const {}
@@ -20,4 +20,5 @@ double Vector::getMagnitude() const {}
 void Vector::print() const {}
 
 Vector::~Vector() {}
+
 
